f8f221bc3b9a7b32c1167b2876a972507bda1511
Calvin Ofori <calvin.kofori@gmail.com>
2025-10-02 14:46:55 +0000

    Add files via upload

---
+++ Digital Clock/script.js
@@ -0,0 +1,21 @@
+// DIGITAL CLOCK PROGRAM
+
+
+function updateClock(){
+
+    const now = new Date();
+    let hour = now.getHours();
+    const meridian = hour >= 12 ? "PM" : "AM" ;
+    hour = hour % 12 || 12 ;
+    hour = hour.toString().padStart(2, 0);
+    const min = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+
+    const timeString = `${hour}:${min}:${seconds} ${meridian}`;
+
+    document.getElementById('clock').textContent = timeString;
+
+}
+
+updateClock();
+setInterval(updateClock, 1000);
